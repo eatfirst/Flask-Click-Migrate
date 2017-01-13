c90a385ddfbf82bf4a7dc74af629929c4e5c3a8d
Jayson Reis <santosdosreis@gmail.com>
2017-01-13 17:02:45 +0100

    CR Fixesˆ

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 setup(
     name='Flask-Click-Migrate',
     version='0.0.1',
-    author='Eat First gmbh',
-    author_email='jayson.reis@eatfirst.com',
+    author='EatFirst.com',
+    author_email='opensource@eatfirst.com',
     description='A library to glue flask + alembic + click',
     url='https://github.com/eatfirst/Flask-Click-Migrate',
     packages=['flask_click_migrate'],
@@ -16,6 +16,9 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     install_requires=[
         'six',
